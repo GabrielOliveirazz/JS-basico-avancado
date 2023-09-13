dbd8ab298486142781a9f171d476579edcb0714e
Gabriel Moura <zkgaab@gmail.com>
2023-09-13 13:52:52 -0300

    aula003

---
+++ aula03/index.js
@@ -0,0 +1,7 @@
+// Todos esses são strings  
+console.log('Gabsz') // '' para usar aspas duplas ("") no texto  
+console.log("Gabsz") // "" para usar aspas simples ('') no texto
+console.log(`Gabsz`) // `´ Templates strings
+
+// Number = Números inteiros ou reias
+console.log(1, 2, 3, 42, 5.6,)
