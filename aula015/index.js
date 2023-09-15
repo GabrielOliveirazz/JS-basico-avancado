5b880a92912801d632254a246894f32059588f61
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-14 22:26:42 -0300

    Create index.js

---
+++ aula015/index.js
@@ -0,0 +1,7 @@
+let num1 = 9.54578
+// let num2 = Math.floor(num1)
+// let num2 = Math.ceil(num1)
+// let num2 = Math.round(num1)
+
+console.log(Math.min(1,2,3,5,6,-3,54,-34,-144))
+console.log(Math.max(1,2,3,5,6,-3,54,-34,-144))
