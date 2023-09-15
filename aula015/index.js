2d77d2e391d7861810615a4b35c084784fea4579
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-15 17:02:40 -0300

    ex015

--- aula015/index.js
+++ aula015/index.js
@@ -1,10 +1,11 @@
-let num1 = 9.54578
+console.log(100 / 0)
+
+
 // let num2 = Math.floor(num1)
 // let num2 = Math.ceil(num1)
 // let num2 = Math.round(num1)
 
-console.log(Math.min(1,2,3,5,6,-3,54,-34,-144))
-console.log(Math.max(1,2,3,5,6,-3,54,-34,-144))
-const aleatorio = Math.round(Math.random() * (10 - 5) + 5)
+//console.log(Math.min(1,2,3,5,6,-3,54,-34,-144))
+//console.log(Math.max(1,2,3,5,6,-3,54,-34,-144))
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5)
 
-console.log(aleatorio)
