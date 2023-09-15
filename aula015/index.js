42a8878b95e1062fa7f2c448232160c013a0f3ef
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-15 15:59:29 -0300

    Update index.js

--- aula015/index.js
+++ aula015/index.js
@@ -5,3 +5,6 @@ let num1 = 9.54578
 
 console.log(Math.min(1,2,3,5,6,-3,54,-34,-144))
 console.log(Math.max(1,2,3,5,6,-3,54,-34,-144))
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5)
+
+console.log(aleatorio)
