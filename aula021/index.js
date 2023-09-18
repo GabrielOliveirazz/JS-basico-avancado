cf9069d392005e726de28812fdf4c833a83c9c0f
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-18 18:14:19 -0300

    Create index.js

---
+++ aula021/index.js
@@ -0,0 +1,16 @@
+/*
+> maior que
+>= maior ou igual que
+< menor que
+<= menor ou igual que
+== igualdade (valor)
+=== igualdade estrita (valor de tipo)
+!= diferente (valor)
+!== diferente estrito (valor escrito)
+*/
+
+
+const num1 = 10
+const num2 = 11
+const comp = num1 < num2
+console.log(comp)
