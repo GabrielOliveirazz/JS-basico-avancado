ff9de29de216a1a9f0b38035e1eeb3ed5346c57b
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-18 13:25:25 -0300

    Update index.js

--- aula019/index.js
+++ aula019/index.js
@@ -1,4 +1,33 @@
 /*
- Primitivos - string - number - boolean - undefined, null (bigint, symbol)
+ Primitivos - string - number - boolean - undefined, null (bigint, symbol) (Copiados)
+
+ Referência (mutavél) - array - object - function (Passados por referência)
 */
 
+const a = {
+    nome: 'Gabriel',
+    sobrenome: 'Moura',
+}
+
+const b = {...a}
+
+console.log(b)
+
+a.nome = 'João'
+console.log(b)
+console.log(a)
+
+
+/*
+let a = [1, 2 ,3]
+let b = [...a]
+
+console.log(a, b)
+
+a.push(4)
+
+console.log(a, b)
+
+b.pop()
+console.log(b, a)
+*/
