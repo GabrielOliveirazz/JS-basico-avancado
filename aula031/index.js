b388e147c89beb04450051138c3d1ca14d2e2458
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-20 22:10:03 -0300

    aula031 e aula032

---
+++ aula031/index.js
@@ -0,0 +1,19 @@
+const vdd = true
+
+// Let tem escopo de bloco 
+
+// 
+
+var nome1 = 'Gab'
+let nome = 'Gab'
+
+if (vdd) {
+    let nome = 'Ray'
+    //console.log(nome, nome2)
+
+
+if (vdd) {
+    let nome = 'Outra coisa'
+    console.log(nome)
+    }
+}
