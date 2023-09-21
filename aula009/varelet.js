e7848a87fec513b04099a95c69b80ae295021510
Gabriel Oliveira <142449789+gabrieloliveiramacedomoura@users.noreply.github.com>
2023-09-21 10:21:28 -0300

    atualizaçao

---
+++ aula009/varelet.js
@@ -0,0 +1,15 @@
+// Dados primitivos                                    
+const nome = 'Rayanne' // String
+const nome1 = "Gabriel" // String
+const nome2 = `Murilo` // String
+
+const num = 10 // Number
+const num1 = 5.9 // Number
+
+let nomeAluno = "" // undefined (Não definido)
+let sobrenomeAluno = null  // Nulo não aponta pra local nenhum na memória  
+
+const aprovado = true // Valor bolean Verdadeiro ou Falso
+const reprovado = false // Valor bolean Verdadeiro ou Falso
+
+
