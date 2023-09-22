ee7df54b8acaec219763b76bb873b4c04fc52e81
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-22 16:00:37 -0300

    Create index.js

---
+++ aula036/index.js
@@ -0,0 +1,14 @@
+//const frutas = ['Pera', 'Maçã' , 'Uva']
+
+//for(let i in frutas)
+//console.log(frutas[i])
+
+const pessoa = {
+    nome: 'Gabriel',
+    sobrenome: 'Oliveira',
+    idade: 16
+}
+
+const chave = 'nome'
+for (let i in pessoa)
+console.log(i)
