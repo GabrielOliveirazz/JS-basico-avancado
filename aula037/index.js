25e24597979c9503cd4fa90ab8eb7c56677cbd35
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-23 11:40:35 -0300

    Update index.js

--- aula037/index.js
+++ aula037/index.js
@@ -1,9 +1,23 @@
-let nome = 'Gabriel Oliveira'
+const nome = 'Gabriel Oliveira'
 
-//for(let i = 0; i < nome.length; i++) {
-  //  console.log(nome[i])
-//}
-
-for (let valor of nome) {
-    console.log(valor)
+for (let i = 0; i < nome.length; i++) {
+  console.log(nome[i])
 }
+
+console.log('#####')
+
+for (let valor of nome) { // pega o valor 
+  console.log(valor)
+}
+
+console.log('#####')
+
+for (let i in nome) { // pega o indice 
+  console.log(nome[i])
+}
+
+console.log('#####')
+
+// For clássico - Geralmente com iteráveis (array ou strings)
+// For in - Retorna o índice ou chave (string, array ou objetos)
+// For of - Retorna o valor em si (interáveis, array ou strings)
