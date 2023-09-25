87fe9eec95b72ad0dc484c6dee968af986cf27d0
Gabriel Moura <zkgaab@gmail.com>
2023-09-25 13:38:08 -0300

    Update index.js

--- aula045/index.js
+++ aula045/index.js
@@ -10,18 +10,28 @@
 */
 
 function retornaHora(data) {
-    if (!(data instanceof Date)) {
-        console.log('n é')
+    if (data && !(data instanceof Date)) {
         throw new TypeError('Esperando intância de Date')
     }
-    if(!data) {
+
+    if (!data) {
         data = new Date()
     }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    })
 }
 
-return data.toLocalTimeString('pt-BR', {
-    hora12: false
-})
-
-const hora = retornaHora()
-console.log(hora)
+try {
+    const data = new Date('01-01-1970 12:58:12')
+    const hora = retornaHora()
+    console.log(hora)
+} catch (e) {
+    // Tratar erro
+} finally {
+    console.log('Tenha um bom dia')
+}
