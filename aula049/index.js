cccb82b98046a5f26b615152c248d280509737b8
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-27 15:57:26 -0300

    Create index.js

---
+++ aula049/index.js
@@ -0,0 +1,28 @@
+// Declaração de função (function hoisting) 
+falaOi()
+function falaOi() {
+    console.log('oie')
+}
+// Firt-class objects (Objetos de primeira classe)0
+
+// Function expression
+const souUmDado = function() {
+    console.log('a')
+}
+souUmDado()
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function')
+}  
+funcaoArrow()
+
+// Dentro de um objeto
+
+const obj = {
+    falar() {
+        console.log('Estou falando')
+    }
+}
+
+obj.falar()
