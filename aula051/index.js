36daa5d672951f74290fc62b82ebfa8f06c13f80
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-28 18:22:01 -0300

    Create index.js

---
+++ aula051/index.js
@@ -0,0 +1,14 @@
+function multiplicador(multiplicador) {
+    function multiplicacao(n) {
+        return n * multiplicador
+    }
+    return n * multiplicador
+}
+
+const duplica = multiplicador(2)
+const triplica = multiplicador(3)
+const quadriplica = multiplicador(4)
+
+console.log(duplica(2))
+console.log(triplica(2))
+console.log(quadriplica(2))
