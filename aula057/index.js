9c86117a4ff47c83d4f9f877a4de39c524ce768e
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-10-03 14:25:40 -0300

    Update index.js

--- aula057/index.js
+++ aula057/index.js
@@ -3,9 +3,21 @@
 // Construtora -> Pessoa (new)
 
 function Pessoa(nome, sobrenome) {
+    // Atributos ou metodos privados
+    const id = 123456
+    const metodoInterno = function() {
+
+    }
+    // Atributos ou metodos públicos
     this.nome = nome
     this.sobrenome = sobrenome
+
+    this.metodo = function() {
+        console.log(this.nome + ' Sou um metodo')
+    }
 }
 
 const p1 = new Pessoa('Gabriel', 'Oliveira')
 const p2 = new Pessoa('Rayanne', 'Nayara')
+
+p2.metodo()
