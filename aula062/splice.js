aca3d699325bcf358b8398617b0a03f1489398fb
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-10-05 20:06:51 -0300

    Update splice.js

---
+++ aula062/splice.js
@@ -0,0 +1,7 @@
+const nomes = ['Maria', 'Rayanne', 'Gabriel', 'Thiago', 'Lucas']
+
+// nomes.splice(indicie, delete, elem1, elem2, elem3)
+// pop
+
+const removidos = nomes.splice(1, 2)
+console.log(nomes, removidos)
