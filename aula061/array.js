793249c954a3b22bea4fe37fd6c6010c5f24160d
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-10-05 17:53:51 -0300

    Create array.js

---
+++ aula061/array.js
@@ -0,0 +1,8 @@
+/*const nomes = ['Eduardo', 'Maria', 'Joana', 'Will', 'Jonas']
+const novo = nomes.slice(1, -2)
+
+console.log(novo)*/
+
+const nomes = ['Gabriel', 'Oliveira', 'Macedo', 'Moura']
+const nome = nomes.join(' ')
+console.log(nome)
