f679eb541f432d5781c8078d0ae7ae2d9b7437c3
Gabriel Moura <zkgaab@gmail.com>
2023-10-10 11:06:47 -0300

    Update index.js

--- aula069/index.js
+++ aula069/index.js
@@ -1,1 +1,13 @@
- 
+const pessoa1 = new Object()
+pessoa1.nome = 'Gabriel'
+pessoa1.sobrenome = 'Oliveira'
+pessoa1.idade = 16
+pessoa1.falarNome = function() {
+    return console.log(`${this.nome} ${this.sobrenome} está falando seu nome e sua idade ${this.idade})
+}
+pessoa1.getDataNascimento = function() {
+    const dataAtual = new Date()
+    return dataAtual.getFullYear() - this.idade
+}
+
+console.log(pessoa1.getDataNascimento())
