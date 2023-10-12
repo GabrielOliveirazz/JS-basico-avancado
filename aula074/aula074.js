24bc3bac6a3e61fbb5a2e178931d0a836f854171
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-10-11 21:04:09 -0300

    Update aula074.js

--- aula074/aula074.js
+++ aula074/aula074.js
@@ -1,1 +1,13 @@
-const a = a
+// new Object -> Object.prototype
+const objA = {
+    chaveA: 'A'
+    // __proto__ : Object.prototype
+}
+
+const objB = {
+    chaveB: 'B'
+    // __proto__ : objA
+}
+
+Object.setPrototypeOf(objB, objA)
+console.log(objB, objA)
