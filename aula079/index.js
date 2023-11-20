9ad34528f9706bab2db1c026a20d113449ccaae1
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-11-20 11:20:09 -0300

    Create index.js

---
+++ aula079/index.js
@@ -0,0 +1,21 @@
+const pessoas = [
+    {id: 3, nome: 'Thiago'},
+    {id: 1, nome: 'Gabriel'},
+    {id: 2, nome: 'Rayanne'},
+]
+
+//const novasPessoas = {}
+//for (const pessoa of pessoas) {
+//    const { id } = pessoa 
+//    novasPessoas[id] = { ...pessoa }
+//}
+
+//console.log(novasPessoas)
+
+const novasPessoas = new Map()
+for (const pessoa of pessoas) {
+    const { id } = pessoa 
+    novasPessoas.set(id,  { ...pessoa })
+}
+
+console.log(novasPessoas)
