eb19f31b260131377e994bc8310ed80c8adaae25
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-11-30 15:49:39 -0300

    Create index.js

---
+++ aula088/index.js
@@ -0,0 +1,32 @@
+function rand(min, max) {
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') {
+            reject('Cai no erro')
+            return
+        }
+        resolve(msg.toUpperCase() + ' - Passe na promise')
+        return
+    }, tempo)
+}
+
+async function executa() {
+    try {
+        const fase1 = await esperaAi('Fase 1', rand())
+        console.log(fase1)
+        const fase2 = await esperaAi('Fase 2', rand())
+        console.log(fase2)
+        const fase3 = await esperaAi('Fase 3', rand())
+        console.log(fase3)
+        console.log('Terminamos a', fase3)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+executa()
